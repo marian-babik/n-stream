fa5279298ec4d266d61a4abedc3bb35b66b3c68c
Marian Babik <marian-babik@users.noreply.github.com>
2025-04-22 16:17:03 +0200

    Update __init__.py

--- nstream/__init__.py
+++ nstream/__init__.py
@@ -1,8 +1,8 @@
 AUTHOR = "Marian Babik"
 AUTHOR_EMAIL = "<marian.babik@cern.ch>"
 COPYRIGHT = "Copyright (C) 2015 CERN"
-VERSION = "0.1.21"
-DATE = "23 Jun 2015"
+VERSION = "0.1.22"
+DATE = "22 Apr 2025"
 __author__ = AUTHOR
 __version__ = VERSION
 __date__ = DATE
